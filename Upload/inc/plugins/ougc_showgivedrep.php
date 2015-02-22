1b1075adb7d69eef5825b615adc7759d9b59fdc4
Omar Gonzalez <Sama34@users.noreply.github.com>
2015-02-22 13:22:36 -0800

    Jump to 1.8

--- Upload/inc/plugins/ougc_showgivedrep.php
+++ Upload/inc/plugins/ougc_showgivedrep.php
@@ -2,38 +2,38 @@
 
 /***************************************************************************
  *
- *   OUGC Show Gived Reputation plugin (/inc/plugins/ougc_showgivedrep.php)
- *	 Author: Omar Gonzalez
- *   Copyright: © 2012 Omar Gonzalez
- *   
- *   Website: http://community.mybb.com/user-25096.html
+ *	OUGC Signature Control plugin (/inc/plugins/ougc_showgivedrep.php)
+ *	Author: Omar Gonzalez
+ *	Copyright: Â© 2012-2014 Omar Gonzalez
  *
- *   Allow users to see their given reputation from their reputation pages.
+ *	Website: http://omarg.me
+ *
+ *	Allow users to see their given reputation from their reputation pages.
  *
  ***************************************************************************
- 
+
 ****************************************************************************
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************/
 
 // Die if IN_MYBB is not defined, for security reasons.
-defined('IN_MYBB') or die('This file cannot be accessed directly.');
+defined('IN_MYBB') or die('Direct initialization of this file is not allowed.');
 
-// Run the ACP hooks.
+// Run/Add Hooks
 if(!defined('IN_ADMINCP') && defined('THIS_SCRIPT') && THIS_SCRIPT == 'reputation.php')
 {
-	global $plugins, $templatelist;
+	global $templatelist;
 
 	$plugins->add_hook('reputation_start', 'ougc_showgivedrep_variable');
 
@@ -49,40 +49,106 @@ if(!defined('IN_ADMINCP') && defined('THIS_SCRIPT') && THIS_SCRIPT == 'reputatio
 	$templatelist .= 'ougc_showgivedrep';
 }
 
-// Array of information about the plugin.
+// Plugin API
 function ougc_showgivedrep_info()
 {
 	global $lang;
-	isset($lang->ougc_showgivedrep) or $lang->load('ougc_showgivedrep', false, true);
+	ougc_showgivedrep_load_language();
 
 	return array(
 		'name'			=> 'OUGC Show Gived Reputation',
-		'description'	=> (isset($lang->ougc_showgivedrep_d) ? $lang->ougc_showgivedrep_d : 'Allow users to see their given reputation from their reputation pages.'),
-		'website'		=> 'http://mods.mybb.com/profile/25096',
-		'author'		=> 'Omar Gonzalez',
-		'authorsite'	=> 'http://community.mybb.com/user-25096.html',
-		'version'		=> '1.0',
-		'guid' 			=> '',
-		'compatibility' => '16*'
+		'description'	=> $lang->ougc_showgivedrep_desc,
+		'website'		=> 'http://omarg.me',
+		'author'		=> 'Omar G.',
+		'authorsite'	=> 'http://omarg.me',
+		'version'		=> '1.8',
+		'versioncode'	=> 1800,
+		'compatibility'	=> '18*'
 	);
 }
 
-// This function runs when the plugin is activated.
+// _activate() routine
 function ougc_showgivedrep_activate()
 {
+	global $cache;
 	ougc_showgivedrep_deactivate();
 
 	require_once MYBB_ROOT.'/inc/adminfunctions_templates.php';
 	find_replace_templatesets('reputation', '#'.preg_quote('show_negative}</option>').'#', 'show_negative}</option>{$ougc_showgivedrep}');
+
+	// Insert/update version into cache
+	$plugins = $cache->read('ougc_plugins');
+	if(!$plugins)
+	{
+		$plugins = array();
+	}
+
+	$info = ougc_showgivedrep_info();
+
+	if(!isset($plugins['showgivedrep']))
+	{
+		$plugins['showgivedrep'] = $info['versioncode'];
+	}
+
+	/*~*~* RUN UPDATES START *~*~*/
+
+	/*~*~* RUN UPDATES END *~*~*/
+
+	$plugins['showgivedrep'] = $info['versioncode'];
+	$cache->update('ougc_plugins', $plugins);
 }
 
-//Deactivate the plugin.
+// _deactivate() routine
 function ougc_showgivedrep_deactivate()
 {
 	require_once MYBB_ROOT.'/inc/adminfunctions_templates.php';
 	find_replace_templatesets('reputation', '#'.preg_quote('{$ougc_showgivedrep}').'#', '', 0);
 }
 
+// _is_installed() routine
+function ougc_showgivedrep_is_installed()
+{
+	global $cache;
+
+	$plugins = (array)$cache->read('ougc_plugins');
+
+	return isset($plugins['showgivedrep']);
+}
+
+// _uninstall() routine
+function ougc_showgivedrep_uninstall($hard=true)
+{
+	global $cache;
+
+	// Delete version from cache
+	$plugins = (array)$cache->read('ougc_plugins');
+
+	if(isset($plugins['showgivedrep']))
+	{
+		unset($plugins['showgivedrep']);
+	}
+
+	if(!empty($plugins))
+	{
+		$cache->update('ougc_plugins', $plugins);
+	}
+	else
+	{
+		$cache->delete('ougc_plugins');
+	}
+}
+
+// Load language if exists
+function ougc_showgivedrep_load_language()
+{
+	global $lang;
+
+	isset($lang->ougc_showgivedrep) or $lang->load('ougc_showgivedrep', false, true);
+
+	isset($lang->ougc_showgivedrep_desc) or $lang->ougc_showgivedrep_desc = 'Allow users to see their given reputation from their reputation pages.';
+}
+
+// Dark magic
 function ougc_showgivedrep_variable()
 {
 	global $mybb;
@@ -103,7 +169,7 @@ function ougc_showgivedrep_variable()
 		isset($lang->ougc_showgivedrep_title) or $lang->ougc_showgivedrep_title = 'Gived Reputations';
 		$lang->comments = $lang->ougc_showgivedrep_title;
 		$show_selected['self'] = ' selected="selected"';
-		ougc_showgivedrep_control_object($GLOBALS['db'], '
+		control_object($GLOBALS['db'], '
 			function query($string, $hide_errors=0, $write_query=0)
 			{
 				static $selfrepdone = false;
@@ -115,9 +181,8 @@ function ougc_showgivedrep_variable()
 				static $selfrepdone2 = false;
 				if(!$selfrepdone2 && !$write_query && strpos($string, \'r.uid\'))
 				{
-					$selfrepdone2 = true;
+					$selfrepdone2 = true;$string2 = $string;
 					$string = str_replace(array(\'r.uid\', \'(u.uid=r.adduid)\'),  array(\'r.adduid\', \'(u.uid=r.uid)\'), preg_replace(\'#r\.uid\=\\\'([0-9]+)\\\'#i\',  \'r.adduid=\\\''.($mybb->user['uid']).'\\\'\', $string));
-					_dump($string);
 				}
 				return parent::query($string, $hide_errors, $write_query);
 			}
@@ -126,52 +191,54 @@ function ougc_showgivedrep_variable()
 		global $plugins;
 
 		$plugins->add_hook('reputation_end', create_function('', 'global $multipage; if($multipage) { $multipage = str_replace(\'show=all\', \'show=self\', $multipage); }'));
+
+		$plugins->add_hook('reputation_vote', create_function('', 'global $postrep_given, $reputation_vote, $lang, $link, $user, $thread_link; if($reputation_vote[\'pid\']) { $postrep_given = $lang->sprintf($lang->postrep_given, $link, $reputation_vote[\'username\'], $thread_link); }'));
 	}
 
-	if(empty($templates->cache['ougc_showgivedrep']))
-	{
-		$templates->cache['ougc_showgivedrep'] = '<option value="self"{$show_selected[\'self\']}>{$lang->ougc_showgivedrep_self}</option>';
-	}
+	isset($templates->cache['ougc_showgivedrep']) or $templates->cache['ougc_showgivedrep'] = '<option value="self"{$show_selected[\'self\']}>{$lang->ougc_showgivedrep_self}</option>';
 
 	eval('$ougc_showgivedrep = "'.$templates->get('ougc_showgivedrep').'";');
 }
 
 // control_object by Zinga Burga from MyBBHacks ( mybbhacks.zingaburga.com ), 1.62
-function ougc_showgivedrep_control_object(&$obj, $code)
+if(!function_exists('control_object'))
 {
-	static $cnt = 0;
-	$newname = '_objcont_'.(++$cnt);
-	$objserial = serialize($obj);
-	$classname = get_class($obj);
-	$checkstr = 'O:'.strlen($classname).':"'.$classname.'":';
-	$checkstr_len = strlen($checkstr);
-	if(substr($objserial, 0, $checkstr_len) == $checkstr)
+	function control_object(&$obj, $code)
 	{
-		$vars = array();
-		// grab resources/object etc, stripping scope info from keys
-		foreach((array)$obj as $k => $v)
+		static $cnt = 0;
+		$newname = '_objcont_'.(++$cnt);
+		$objserial = serialize($obj);
+		$classname = get_class($obj);
+		$checkstr = 'O:'.strlen($classname).':"'.$classname.'":';
+		$checkstr_len = strlen($checkstr);
+		if(substr($objserial, 0, $checkstr_len) == $checkstr)
 		{
-			if($p = strrpos($k, "\0"))
+			$vars = array();
+			// grab resources/object etc, stripping scope info from keys
+			foreach((array)$obj as $k => $v)
 			{
-				$k = substr($k, $p+1);
-			}
-			$vars[$k] = $v;
-		}
-		if(!empty($vars))
-		{
-			$code .= '
-				function ___setvars(&$a) {
-					foreach($a as $k => &$v)
-						$this->$k = $v;
+				if($p = strrpos($k, "\0"))
+				{
+					$k = substr($k, $p+1);
 				}
-			';
+				$vars[$k] = $v;
+			}
+			if(!empty($vars))
+			{
+				$code .= '
+					function ___setvars(&$a) {
+						foreach($a as $k => &$v)
+							$this->$k = $v;
+					}
+				';
+			}
+			eval('class '.$newname.' extends '.$classname.' {'.$code.'}');
+			$obj = unserialize('O:'.strlen($newname).':"'.$newname.'":'.substr($objserial, $checkstr_len));
+			if(!empty($vars))
+			{
+				$obj->___setvars($vars);
+			}
 		}
-		eval('class '.$newname.' extends '.$classname.' {'.$code.'}');
-		$obj = unserialize('O:'.strlen($newname).':"'.$newname.'":'.substr($objserial, $checkstr_len));
-		if(!empty($vars))
-		{
-			$obj->___setvars($vars);
-		}
+		// else not a valid object or PHP serialize has changed
 	}
-	// else not a valid object or PHP serialize has changed
 }
